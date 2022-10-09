6c7da906e8ff6bb59999509ee6454380515b47aa
Vitalii Boiko <107396176+vitaliiseeker@users.noreply.github.com>
2022-10-10 01:43:00 +0300

    corrected code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-// import { ThemeProvider } from 'styled-components';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,13 +10,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 );
 
 
-// import theme from './theme';
-// import { GlobalStyle } from 'theme/GlobalStyle';
-
-//   <React.StrictMode>
-//     <ThemeProvider theme={theme}>
-//       <GlobalStyle />
-//       <App />
-//     </ThemeProvider>
-//   </React.StrictMode>
-// );
